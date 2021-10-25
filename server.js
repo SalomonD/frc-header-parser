6cfba120a7cf483cd64ff83a878ea491161bb1db
Salomon NSSO <salomonnssow10@gmail.com>
2021-10-25 17:47:55 +0100

    add endpoint to return information about who send requested the server

--- server.js
+++ server.js
@@ -19,6 +19,14 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get("/api/whoami", function (req, res) {
+  const ipaddress = req.ip;
+  const language = req.headers["accept-language"];
+  const software = req.headers['user-agent'];
+
+  res.json({ipaddress, language, software})
+});
+
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
@@ -28,6 +36,6 @@ app.get("/api/hello", function (req, res) {
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
